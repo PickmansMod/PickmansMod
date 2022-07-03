16e8c3076d1f8ddf2ab9d7773ae2683298740124
Pickmansmod <jehernandez199670@gmail.com>
2022-07-03 18:15:12 -0500

    terminando meses.js

--- JavaScript/Ejercicios/meses.js
+++ JavaScript/Ejercicios/meses.js
@@ -1,13 +1,40 @@
-var mes = "Enero";
+var mes = "Diciembre";
 
 switch (mes){
     case "Enero":
         console.log("El mes de", mes ,"tiene 31 dias")
         break;
     case "Febrero":
-            console.log("El mes de", mes ,"tiene 28 dias")
-            break;
+        console.log("El mes de", mes ,"tiene 28 dias")
+        break;
     case "Marzo":
-            console.log("El mes de", mes ,"tiene 31 dias")
-            break;   
-}
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;   
+    case "Abril":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;
+    case "Mayo":
+        console.log("El mes de", mes ,"tiene 28 dias")
+        break;
+    case "Junio":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;
+    case "Julio":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;
+    case "Agosto":
+        console.log("El mes de", mes ,"tiene 28 dias")
+        break;
+    case "Septiembre":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;
+    case "Octubre":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;
+    case "Noviembre":
+        console.log("El mes de", mes ,"tiene 28 dias")
+        break;
+    case "Diciembre":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;          
+}        
