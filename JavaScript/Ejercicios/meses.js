0ec37ba54225db281f4d1980eb8bcf98b4032e71
Pickmansmod <jehernandez199670@gmail.com>
2022-07-03 14:33:18 -0500

    añadido algunos meses

---
+++ JavaScript/Ejercicios/meses.js
@@ -0,0 +1,13 @@
+var mes = "Enero";
+
+switch (mes){
+    case "Enero":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;
+    case "Febrero":
+            console.log("El mes de", mes ,"tiene 28 dias")
+            break;
+    case "Marzo":
+            console.log("El mes de", mes ,"tiene 31 dias")
+            break;   
+}
