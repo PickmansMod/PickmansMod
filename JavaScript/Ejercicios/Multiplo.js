6f0e45bb56208881b1cfb6b42f6e8104550e5c40
Jorge Hernandez <52835183+PickmansMod@users.noreply.github.com>
2022-07-03 19:22:06 -0500

    añadido meses

--- JavaScript/Ejercicios/Multiplo.js
+++ JavaScript/Ejercicios/Multiplo.js
@@ -1,8 +1,40 @@
-var primero = 20;
-var segundo = 2;
+var mes = "Diciembre";
 
-if (primero % segundo == 0) {
-    console.log("%d es multiplo de", primero, segundo)
-} else {
-    console.log("%d no es multiplo de", primero, segundo)
-}
+switch (mes){
+    case "Enero":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;
+    case "Febrero":
+        console.log("El mes de", mes ,"tiene 28 dias")
+        break;
+    case "Marzo":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;   
+    case "Abril":
+        console.log("El mes de", mes ,"tiene 30 dias")
+        break;
+    case "Mayo":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;
+    case "Junio":
+        console.log("El mes de", mes ,"tiene 30 dias")
+        break;
+    case "Julio":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;
+    case "Agosto":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;
+    case "Septiembre":
+        console.log("El mes de", mes ,"tiene 30 dias")
+        break;
+    case "Octubre":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;
+    case "Noviembre":
+        console.log("El mes de", mes ,"tiene 30 dias")
+        break;
+    case "Diciembre":
+        console.log("El mes de", mes ,"tiene 31 dias")
+        break;          
+}   
