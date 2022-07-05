c25d876967c35fc283fea295e28d570cc8241ed6
Pickmansmod <jehernandez199670@gmail.com>
2022-07-04 19:24:37 -0500

    bug fixed

--- JavaScript/Ejercicios/tree.js
+++ JavaScript/Ejercicios/tree.js
@@ -1,9 +1,9 @@
-var floor = 5;
+var floor = 3;
 var floor_math = 2 * floor + 1;
 
-for (let i = 0; i < floor_math; i++){
+for (let i = 0; i < floor_math ; i++){
     if (i % 2 === 1){
-        console.log(" ".repeat(Math.trunc((floor - i) / 2)) + "+".repeat(i) + " ".repeat(Math.trunc((floor - i) / 2)))
+        console.log(" ".repeat((floor_math - i) / 2) + "*".repeat(i) + " ".repeat((floor_math - i) / 2))
     }
 }
 
