719eab696b9176cf153349764cfcdbd26ae525c4
Pickmansmod <jehernandez199670@gmail.com>
2022-07-04 19:06:07 -0500

    hola

--- JavaScript/Ejercicios/meses.js
+++ JavaScript/Ejercicios/meses.js
@@ -11,28 +11,28 @@ switch (mes){
         console.log("El mes de", mes ,"tiene 31 dias")
         break;   
     case "Abril":
-        console.log("El mes de", mes ,"tiene 31 dias")
+        console.log("El mes de", mes ,"tiene 30 dias")
         break;
     case "Mayo":
-        console.log("El mes de", mes ,"tiene 28 dias")
+        console.log("El mes de", mes ,"tiene 31 dias")
         break;
     case "Junio":
-        console.log("El mes de", mes ,"tiene 31 dias")
+        console.log("El mes de", mes ,"tiene 30 dias")
         break;
     case "Julio":
         console.log("El mes de", mes ,"tiene 31 dias")
         break;
     case "Agosto":
-        console.log("El mes de", mes ,"tiene 28 dias")
+        console.log("El mes de", mes ,"tiene 31 dias")
         break;
     case "Septiembre":
-        console.log("El mes de", mes ,"tiene 31 dias")
+        console.log("El mes de", mes ,"tiene 30 dias")
         break;
     case "Octubre":
         console.log("El mes de", mes ,"tiene 31 dias")
         break;
     case "Noviembre":
-        console.log("El mes de", mes ,"tiene 28 dias")
+        console.log("El mes de", mes ,"tiene 30 dias")
         break;
     case "Diciembre":
         console.log("El mes de", mes ,"tiene 31 dias")
