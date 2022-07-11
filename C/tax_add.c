8a3225528ac36404ab6f957178176f5442cd9296
Pickmansmod <jehernandez199670@gmail.com>
2022-07-11 14:07:29 -0500

    fix float

---
+++ C/tax_add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float tax, amount, tax_added;
+    
+    printf("Enter an amount: ");
+    scanf("%f", &amount);
+    printf("Enter the tax: ");
+    scanf("%f", tax);
+    tax_added = amount + amout * (tax / 100);
+    printf("With tax added: ", tax_added);
+    return 0;
+}
